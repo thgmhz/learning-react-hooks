8174375daa951c05fae2fbf7c2b1c41cc9ece221
Thiago <thiagomunhozmedeiros@gmail.com>
2019-05-11 12:48:59 -0300

    charPicker: update to use "useHttp" custom hook

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import useHttp from '../hooks/http'
 
 import './CharPicker.css'
 
@@ -7,45 +8,21 @@ const CharPicker = ({
   selectedChar,
   side,
 }) => {
-  const [loadedCharacters, setCharacters] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, fetchedData] = useHttp('https://swapi.co/api/people', [])
 
-  const componentDidMount = () => {
-    setIsLoading(true)
-
-    fetch('https://swapi.co/api/people')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch.')
-        }
-        
-        return response.json()
-      })
-      .then(charData => {
-        const selectedCharacters = charData.results.slice(0, 5)
-
-        const charactersArray = selectedCharacters.map((char, index) => ({
-          name: char.name,
-          id: index + 1
-        }))
-
-        setIsLoading(false)
-        setCharacters(charactersArray)
-      })
-      .catch(err => {
-        console.log(err)
-        setIsLoading(false)
-      })
-  }
-
-  useEffect(componentDidMount, []) 
+  const selectedCharacters = fetchedData 
+    ? fetchedData.results.slice(0, 5).map((char, index) => ({
+        name: char.name,
+        id: index + 1
+      }))
+    : []
 
   let content = <p>Loading characters...</p>
 
   if (
     !isLoading &&
-    loadedCharacters &&
-    loadedCharacters.length > 0
+    selectedCharacters &&
+    selectedCharacters.length > 0
   ) {
     content = (
       <select
@@ -53,7 +30,7 @@ const CharPicker = ({
         value={selectedChar}
         className={side}
       >
-        {loadedCharacters.map(char => (
+        {selectedCharacters.map(char => (
           <option key={char.id} value={char.id}>
             {char.name}
           </option>
@@ -62,7 +39,7 @@ const CharPicker = ({
     )
   } else if (
     !isLoading &&
-    (!loadedCharacters || loadedCharacters.length === 0)
+    (!selectedCharacters || selectedCharacters.length === 0)
   ) {
     content = <p>Could not fetch any data.</p>
   }
